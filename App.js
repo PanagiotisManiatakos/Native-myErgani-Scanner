63e4cd6859cdf7c36388d78bbe660a2a2da59229
Panagiotis Maniatakos <panagiwtismaniatakos@gmail.com>
2022-11-30 18:21:17 +0200

    delete redux and implement React Contect API

--- App.js
+++ App.js
@@ -1,19 +1,34 @@
 import React from "react";
-import { Provider } from "react-redux";
-import store from "./src/redux/store";
 import AppNav from "./src/navigation/AppNav";
 import { SafeAreaView, View } from "react-native";
 import About from "./src/components/ui/About";
-import BackWaves from "./src/assets/BackWaves";
+import "./styles";
+import { ReduxContext } from "./src/Context";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 function App() {
+  const [session, setSession] = React.useState(null);
+  const [login, setLogin] = React.useState(null);
+  const [scan, setScan] = React.useState(undefined);
+
+  React.useEffect(() => {
+    const initRedux = async () => {
+      await AsyncStorage.getItem("session").then((d) => setSession(d));
+      await AsyncStorage.getItem("login").then((d) => setLogin(d));
+      await AsyncStorage.getItem("scan").then((d) => setScan(d));
+    };
+
+    initRedux();
+  }, []);
+
+  const context = { session, setSession, login, setLogin, scan, setScan };
   return (
-    <View style={{ flex: 1, backgroundColor: "#212529" }}>
-      <SafeAreaView style={{ flex: 1 }}>
-        <Provider store={store}>
+    <View className="flex-1 bg-gray-900">
+      <SafeAreaView className="flex-1">
+        <ReduxContext.Provider value={context}>
           <AppNav />
-        </Provider>
-        <About />
+          <About />
+        </ReduxContext.Provider>
       </SafeAreaView>
     </View>
   );
--- src/redux/actions.js
+++
@@ -1,9 +0,0 @@
-export const SetLogin = (value) => ({
-  type: "SET_LOGIN",
-  value,
-});
-
-export const SetSession = (value) => ({
-  type: "SET_SESSION",
-  value,
-});
